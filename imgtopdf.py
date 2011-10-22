e589c8f56f997344b28273a17481f41d46879971
joulesm <joulesm@gmail.com>
2011-10-22 01:48:33 -0400

    updated server

--- imgtopdf.py
+++ imgtopdf.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory
+import json
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = './uploads'
@@ -7,6 +8,7 @@ ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
 
 # ensuring the file extensions are allowed
 def allowed_file(filename):
@@ -23,12 +25,18 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename = filename))
+            upload_url = url_for('uploaded_file', filename = filename)
+            full_url = 'http://red.xvm.mit.edu:5001' + upload_url
+            json_url1 = '{"url": "' + full_url + '"}'
+            json_url = "'" + json_url1 + "'"
+            #return redirect(upload_url)
+            #return render_template('uploaded.html', url=full_url)
+            return json_url
     
     return '''
     <!doctype html>
-    <title>Upload File</title>
-    <h1> Upload File</h1>
+    <title>ImagetoPDF</title>
+    <h1>Image to PDF</h1>
     <form action="" method=post enctype=multipart/form-data>
        <p><input type=file name=file>
           <input type=submit value=Upload>
